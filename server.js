35d512563f0f8164a7db4b1d4274ed71260feeb4
Stebanchavez <csteban344@gmail.com>
2025-02-09 20:31:02 -0700

    modified:   server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const logger = require("morgan"); 
 const Dog = require('./models/Dog');
-const dogRoutes = require("./routes/dogs");
+//const dogRoutes = require("./routes/dogs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -25,11 +25,42 @@ app.use(cors())
 
 
 // Routes
-app.use("/dogs", dogRoutes);
+//app.use("/dogs", dogRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Dog API!');
   });
 
+app.get('/dogs', async (req, res) => {
+    try {
+      const dogs = await Dog.find();
+      res.json(dogs);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  
+  
+app.post('/dogs', async (req, res) => {
+    const { name, breed, image } = req.body;
+    const newDog = new Dog({ name, breed, image });
+  
+    try {
+      await newDog.save();
+      res.status(201).json(newDog);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  });
+  
+  // DELETE route
+  app.delete('/dogs/:id', async (req, res) => {
+    try {
+      await Dog.findByIdAndDelete(req.params.id);
+      res.status(200).send('Dog deleted');
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
