40ec7a387e654b8cd8b9db3a6e3fe7823f25e4ab
Stebanchavez <csteban344@gmail.com>
2025-02-09 18:40:18 -0700

    fixing app crash

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI,)
 
 // check connection
 mongoose.connection.on('connected', () => {
